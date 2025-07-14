c7f55e53221c91d4ca9107ce68188f347facb48c
patrik <patrik.biblak@gmail.com>
2025-07-14 14:07:44 +0200

    Projects 2 content made

--- src/pages/ProjectsNone.js
+++ src/pages/ProjectsNone.js
@@ -1,7 +1,18 @@
 import "../styles/projectsnone.css";
 
 const ProjectsNone = () => {
+    return (
+        <div className="projects-none container">
+            <h1>Moje projekty</h1>
+            <p>Na tejto sekcii momentálne pracujem. Medzitým si môžete pozrieť môj profil alebo ma kontaktovať.</p>
 
+            <div className="box-message">
+                <h4>Každý expert niekde začínal..</h4>
+                <p>Pracujem na portfóliu a budujem základy silného mena.</p>
+                <p>Chcete sa stať súčasťou mojej cesty? Ozvite sa.</p>
+            </div>
+        </div>
+    )
 }
 
 export default ProjectsNone;
