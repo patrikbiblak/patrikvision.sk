183907d50e58ad32df5dac9edfe7b3c970a2154d
patrik <patrik.biblak@gmail.com>
2025-07-14 13:24:50 +0200

    Build + contact page update

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -23,7 +23,7 @@ const ContactPage = () => {
   };
 
   const contactDetails = [
-    { icon: Mail, label: 'Email', value: 'patrik.biblak@gmail.com' },
+    { icon: Mail, label: 'Email', value: 'contact@patrikvision.sk' },
     { icon: Phone, label: 'Telefon', value: '+421 918 755 276' },
     { icon: MapPin, label: 'Poloha', value: 'Slovensko' },
   ];
