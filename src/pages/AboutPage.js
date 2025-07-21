de6f6dbf5070b2ce0760eff15f0ff5b90af757d1
patrik <patrik.biblak@gmail.com>
2025-07-21 13:12:41 +0200

    About-me page heading redesign

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -6,7 +6,7 @@ const AboutPage = () => {
     return (
         <div className="about-page container">
             <div className="aboutme-heading">
-                <h1>Kto som</h1>
+                <h1>Kto <span>som</span></h1>
                 <p>Som mladý a nadšený front-end developer ktorý kladie dôraz na transparentnú komunikáciu, dodržiavanie termínov a vysokú kvalitu.</p>
                 <p>Vytváram rýchle a responzívne webové stránky presne podľa vašich predstáv, ktoré plynulo fungujú na rôznych zariadeniach. Môžete sa na mňa spoľahnúť, že vaše nápady premením na pútavú online prezentáciu, ktorá osloví vašich návštevníkov.</p>
                 <p>Potrebujete niekoho, kto zvládne developerské šprinty? Ste na správnom mieste!</p>
