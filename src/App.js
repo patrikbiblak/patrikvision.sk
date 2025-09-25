d4775eef8fb211300425d93c4c7ba946fea04553
patrik <patrik.biblak@gmail.com>
2025-09-25 11:47:21 +0200

    subpages removed temporarly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React, { Suspense } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import useVersionChecker from "./hooks/useVersionChecker";
-import AboutPage from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
-import ProjectsNone from "./pages/ProjectsNone";
-import ServicesPage from "./pages/ServicesPage";
-import ContactPage from "./pages/ContactPage";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 import PrivacyPolicy from "./components/PrivacyPolicy";
@@ -26,10 +22,10 @@ const App = () => {
             <main>
               <Routes>
                 <Route path="/" element={<HomePage />}/>
-                <Route path="/about" element={<AboutPage />}/>
-                <Route path="/portfolio" element={<ProjectsNone />}/>
-                <Route path="/services" element={<ServicesPage />}/>
-                <Route path="/contact" element={<ContactPage />}/>
+                <Route path="/about" element={<Navigate to="/" replace />}/>
+                <Route path="/portfolio" element={<Navigate to="/" replace />}/>
+                <Route path="/services" element={<Navigate to="/" replace />}/>
+                <Route path="/contact" element={<Navigate to="/" replace />}/>
                 <Route path="/gdpr" element={<PrivacyPolicy />}/>
               </Routes>
             </main>
